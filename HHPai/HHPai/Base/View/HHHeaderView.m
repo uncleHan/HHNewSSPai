aa77401d8acae0719eac2668ac3475ff61678f4f
uncleHan <hanheng1124@126.com>
2018-01-24 19:16:14 +0800

    添加HHHeaderView

---
+++ HHPai/HHPai/Base/View/HHHeaderView.m
@@ -0,0 +1,22 @@
+//
+//  HHHeaderView.m
+//  HHPai
+//
+//  Created by 恒 韩 on 2018/1/24.
+//  Copyright © 2018年 co. All rights reserved.
+//
+
+#import "HHHeaderView.h"
+
+@interface HHHeaderView()
+
+@property (nonatomic,strong)UILabel *titleLabel;
+@property (nonatomic,strong)UIButton *button;
+
+@end
+
+@implementation HHHeaderView
+
+
+
+@end
